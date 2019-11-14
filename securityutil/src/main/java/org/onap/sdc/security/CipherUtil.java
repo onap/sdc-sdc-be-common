6fad0d11b81bfea098c5f6f7669ce4e43389c115
Yuli Shlosberg <ys9693@att.com>
2019-11-14 15:25:32 +0200

    add securityUtil code to Onap
    Issue-ID: SDC-2656
    Signed-off-by: Yuli Shlosberg <ys9693@att.com>
    Change-Id: Iae1a78f0960386e9bdc0994fbac09a24e041cb54

---
+++ securityutil/src/main/java/org/onap/sdc/security/CipherUtil.java
@@ -0,0 +1,142 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.sdc.security;
+
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.ArrayUtils;
+import org.onap.sdc.security.logging.elements.ErrorLogOptionalData;
+import org.onap.sdc.security.logging.elements.LogFieldsMdcHandler;
+import org.onap.sdc.security.logging.enums.EcompLoggerErrorCode;
+import org.onap.sdc.security.logging.wrappers.Logger;
+
+public class CipherUtil {
+    private static Logger log = Logger.getLogger( CipherUtil.class.getName());
+    private static final String ALGORITHM = "AES";
+    private static final String ALGORYTHM_DETAILS = ALGORITHM + "/CBC/PKCS5PADDING";
+    private static final String CIPHER_PROVIDER = "SunJCE";
+    private static final int BLOCK_SIZE = 128;
+    private static final int BYTE_SIZE = 8;
+    private static final int IV_SIZE = BLOCK_SIZE / BYTE_SIZE;
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    private static final String ALGORITHM_NAME = "SHA1PRNG";
+
+    /**
+     * Encrypt the text using the secret key in key.properties file
+     *
+     * @param value string to encrypt
+     * @return The encrypted string
+     * @throws CipherUtilException
+     *             In case of issue with the encryption
+     */
+    public static String encryptPKC(String value, String base64key) throws CipherUtilException {
+        Cipher cipher;
+        byte[] iv = new byte[IV_SIZE];
+        byte[] finalByte;
+        try {
+            cipher = Cipher.getInstance(ALGORYTHM_DETAILS, CIPHER_PROVIDER);
+            SecureRandom secureRandom = SecureRandom.getInstance(ALGORITHM_NAME);
+            secureRandom.nextBytes(iv);
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKeySpec(base64key), ivspec);
+            finalByte = cipher.doFinal(value.getBytes());
+
+        } catch (Exception ex) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(), "encrypt failed", ex);
+            throw new CipherUtilException(ex);
+        }
+        return Base64.encodeBase64String(addAll(iv, finalByte));
+    }
+
+    /**
+     * Decrypts the text using the secret key in key.properties file.
+     *
+     * @param message
+     *            The encrypted string that must be decrypted using the ONAP Portal
+     *            Encryption Key
+     * @return The String decrypted
+     * @throws CipherUtilException
+     *             if any decryption step fails
+     */
+
+    public static String decryptPKC(String message, String base64key) throws CipherUtilException {
+        byte[] encryptedMessage = Base64.decodeBase64(message);
+        Cipher cipher;
+        byte[] decrypted;
+        try {
+            cipher = Cipher.getInstance(ALGORYTHM_DETAILS, CIPHER_PROVIDER);
+            IvParameterSpec ivspec = new IvParameterSpec(subarray(encryptedMessage, 0, IV_SIZE));
+            byte[] realData = subarray(encryptedMessage, IV_SIZE, encryptedMessage.length);
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKeySpec(base64key), ivspec);
+            decrypted = cipher.doFinal(realData);
+
+        } catch (Exception ex) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(),"decrypt failed", ex);
+            throw new CipherUtilException(ex);
+        }
+        return new String(decrypted);
+    }
+
+    private static SecretKeySpec getSecretKeySpec(String keyString) {
+        byte[] key = Base64.decodeBase64(keyString);
+        return new SecretKeySpec(key, ALGORITHM);
+    }
+
+    private static byte[] addAll(byte[] array1, byte[] array2) {
+        if (array1 == null) {
+            return ArrayUtils.clone(array2);
+        } else if (array2 == null) {
+            return ArrayUtils.clone(array1);
+        } else {
+            byte[] joinedArray = new byte[array1.length + array2.length];
+            System.arraycopy(array1, 0, joinedArray, 0, array1.length);
+            System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
+            return joinedArray;
+        }
+    }
+
+    private static byte[] subarray(byte[] array, int startIndexInclusive, int endIndexExclusive) {
+        if (array == null) {
+            return null;
+        } else {
+            if (startIndexInclusive < 0) {
+                startIndexInclusive = 0;
+            }
+
+            if (endIndexExclusive > array.length) {
+                endIndexExclusive = array.length;
+            }
+
+            int newSize = endIndexExclusive - startIndexInclusive;
+            if (newSize <= 0) {
+                return EMPTY_BYTE_ARRAY;
+            } else {
+                byte[] subarray = new byte[newSize];
+                System.arraycopy(array, startIndexInclusive, subarray, 0, newSize);
+                return subarray;
+            }
+        }
+    }
+}
---
+++ securityutil/src/main/java/org/onap/sdc/security/filters/SessionValidationFilter.java
@@ -0,0 +1,217 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.sdc.security.filters;
+
+import org.onap.sdc.security.*;
+import org.onap.sdc.security.logging.elements.ErrorLogOptionalData;
+import org.onap.sdc.security.logging.elements.LogFieldsMdcHandler;
+import org.onap.sdc.security.logging.enums.EcompLoggerErrorCode;
+import org.onap.sdc.security.logging.wrappers.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public abstract class SessionValidationFilter implements Filter {
+    private static final Logger log = Logger.getLogger(SessionValidationFilter.class.getName());
+    private ISessionValidationFilterConfiguration filterConfiguration;
+    private List<String> excludedUrls;
+
+    public abstract ISessionValidationFilterConfiguration getFilterConfiguration();
+    protected abstract Cookie addRoleToCookie(Cookie updatedCookie) throws RedirectException;
+    protected abstract void authorizeUserOnSessionExpiration(AuthenticationCookie authenticationCookie, Cookie[] cookies) throws RedirectException;
+    protected abstract void handleRedirectException(HttpServletResponse httpResponse) throws IOException;
+
+    @Override
+    public final void init(FilterConfig filterConfig) {
+        filterConfiguration = getFilterConfiguration();
+        excludedUrls = filterConfiguration.getExcludedUrls();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        final HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        long starTime = System.nanoTime();
+        if (log.isDebugEnabled()) {
+            log.debug("SessionValidationFilter: Validation started, received request with URL {}", httpRequest.getRequestURL());
+        }
+
+        try {
+            if (isValidationRequired(servletRequest, servletResponse, filterChain, httpRequest)){
+                List<Cookie> cookies = extractAuthenticationCookies(httpRequest.getCookies());
+                if (log.isDebugEnabled()) {
+                    log.debug("doFilter: the cookies we received from extractAuthenticationCookies, before processRequest: {}", cookies);
+                }
+
+                Cookie extractedCookie = cookies.get(0);
+                processRequest(httpRequest, extractedCookie);
+
+                // response processing
+                if (log.isDebugEnabled()) {
+                    log.debug("SessionValidationFilter: Cookie from request {} is valid, passing request to session extension ...", httpRequest.getRequestURL());
+                }
+                Cookie updatedCookie = processResponse(extractedCookie);
+
+                cleanResponseFromLeftoverCookies(httpResponse, cookies);
+
+                if (log.isDebugEnabled()) {
+                    log.debug("SessionValidationFilter: request {} passed all validations, passing request to endpoint ...", httpRequest.getRequestURL());
+                }
+                httpResponse.addCookie(updatedCookie);
+                filterChain.doFilter(servletRequest, httpResponse);
+            }
+        } catch (RedirectException e) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(),"Exception is thrown while authenticating cookie: {}", e.getMessage());
+            handleRedirectException(httpResponse);
+        }
+        long durationSec = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - starTime);
+        long durationMil = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - starTime);
+        if (log.isDebugEnabled()) {
+            log.debug("SessionValidationFilter: Validation ended, running time for URL {} is: {} seconds {} milliseconds", httpRequest.getPathInfo(), durationSec, durationMil);
+        }
+    }
+
+
+    private boolean isValidationRequired(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain, HttpServletRequest httpRequest) throws IOException, ServletException, RedirectException {
+        if (isUrlFromWhiteList(httpRequest)) {
+            log.debug("SessionValidationFilter: URL {} excluded from access validation , passing request to endpoint ... ", httpRequest.getRequestURL());
+            filterChain.doFilter(servletRequest, servletResponse);
+            return false;
+
+        } else if (!isCookiePresent(httpRequest.getCookies())) {
+            //redirect to portal app
+            log.debug("SessionValidationFilter: Cookie from request {} is not valid, redirecting request to portal", httpRequest.getRequestURL());
+            throw new RedirectException(String.format("Cookie from request %s is not valid", httpRequest.getRequestURL() ));
+        }
+        return true;
+    }
+
+    private void processRequest(HttpServletRequest httpRequest, Cookie cookie) throws RedirectException {
+        AuthenticationCookie authenticationCookie;
+        try {
+            authenticationCookie = AuthenticationCookieUtils.getAuthenticationCookie(cookie, filterConfiguration);
+        }
+        catch (CipherUtilException e) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(),"SessionValidationFilter: Cookie decryption error : {}", e.getMessage());
+            throw new RedirectException("Cookie decryption error");
+        }
+        if (AuthenticationCookieUtils.isSessionExpired(authenticationCookie, filterConfiguration)) {
+            if (log.isDebugEnabled()) {
+                log.debug("SessionValidationFilter:session is expired for URL {}", httpRequest.getRequestURL());
+            }
+            authorizeUserOnSessionExpiration(authenticationCookie, httpRequest.getCookies());
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("SessionValidationFilter: Role is valid");
+        }
+    }
+
+    private Cookie processResponse(Cookie cookie) throws IOException, RedirectException {
+        Cookie updatedCookie;
+        try {
+            updatedCookie = AuthenticationCookieUtils.updateSessionTime(cookie, filterConfiguration);
+        } catch (CipherUtilException e) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(),"SessionValidationFilter: Cookie cipher error {}", e.getMessage());
+            if (log.isDebugEnabled()) {
+                log.debug("SessionValidationFilter: Cookie cipher error : {}", e.getMessage(), e);
+            }
+            throw new RedirectException(e);
+        }
+        return addRoleToCookie(updatedCookie);
+    }
+
+    private boolean isCookiePresent(Cookie[] cookies) {
+        if (cookies == null) {
+            return false;
+        }
+        String actualCookieName = filterConfiguration.getCookieName();
+        boolean isPresent = Arrays.stream(cookies).anyMatch(c -> isCookieNameMatch(actualCookieName, c));
+        if (!isPresent) {
+            log.error(EcompLoggerErrorCode.BUSINESS_PROCESS_ERROR, LogFieldsMdcHandler.getInstance().getServiceName(), new ErrorLogOptionalData(),"SessionValidationFilter: Session Validation Cookie missing ");
+            return false;
+        }
+        return true;
+    }
+
+    private List<Cookie> extractAuthenticationCookies(Cookie[] cookies) {
+        String actualCookieName = filterConfiguration.getCookieName();
+        if (log.isDebugEnabled()) {
+            Arrays.stream(cookies).forEach(c->log.debug("SessionValidationFilter: Cookie name {}", c.getName()));
+            log.debug("SessionValidationFilter: Extracting authentication cookies, {} cookies in request", cookies.length);
+        }
+        List<Cookie> authenticationCookies = Arrays.stream(cookies).filter(c -> isCookieNameMatch(actualCookieName, c)).collect(Collectors.toList());
+        if (log.isDebugEnabled()) {
+            log.debug("SessionValidationFilter: Extracted {} authentication cookies from request", authenticationCookies.size());
+        }
+        if( authenticationCookies.size() > 1 && log.isDebugEnabled()) {
+            authenticationCookies.forEach( cookie -> log.debug("SessionValidationFilter: Multiple cookies found cookie name, {} cookie value {}", cookie.getName(), cookie.getValue()));
+        }
+        return authenticationCookies;
+    }
+
+
+    // use contains for matching due issue with ecomp portal ( change cookie name, add prefix ), temp solution
+    private boolean isCookieNameMatch(String actualCookieName, Cookie c) {
+        return c.getName().contains(actualCookieName);
+    }
+
+    private boolean isUrlFromWhiteList(HttpServletRequest httpRequest) {
+        if (httpRequest.getPathInfo() == null){
+            final String servletPath = httpRequest.getServletPath().toLowerCase();
+            log.debug("SessionValidationFilter: pathInfo is null, trying to check by servlet path white list validation -> ServletPath: {} ", servletPath);
+            return excludedUrls.stream().
+                    anyMatch(servletPath::matches);
+        }
+        String pathInfo = httpRequest.getPathInfo().toLowerCase();
+        log.debug("SessionValidationFilter: white list validation ->  PathInfo: {} ", pathInfo);
+        return excludedUrls.stream().
+                anyMatch(pathInfo::matches);
+    }
+
+    private void cleanResponseFromLeftoverCookies(HttpServletResponse httpResponse, List<Cookie> cookiesList) {
+        for (Cookie cookie:cookiesList){
+            Cookie cleanCookie = AuthenticationCookieUtils.createUpdatedCookie(cookie, null, filterConfiguration);
+            cleanCookie.setMaxAge(0);
+            if (log.isDebugEnabled()) {
+                log.debug("SessionValidationFilter Cleaning Cookie cookie name: {} added to response", cleanCookie.getName());
+            }
+            httpResponse.addCookie(cleanCookie);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
