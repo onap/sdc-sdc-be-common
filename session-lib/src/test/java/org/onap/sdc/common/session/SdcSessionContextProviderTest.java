27aeaae244bb6628a3e95a409b25732c9cd7d630
shrek2000 <oren.kleks@amdocs.com>
2019-11-27 07:33:43 +0200

    Unit test
    Issue-ID: SDC-2690
    Signed-off-by: shrek2000 <oren.kleks@amdocs.com>
    Change-Id: I25374d786669b427f8573665a9b176b66cb76233
    Signed-off-by: shrek2000 <oren.kleks@amdocs.com>

---
+++ session-lib/src/test/java/org/onap/sdc/common/session/SdcSessionContextProviderTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2019 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.sdc.common.session;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.sdc.common.session.impl.SdcSessionContextProvider;
+import org.onap.sdc.common.session.impl.SessionException;
+
+public class SdcSessionContextProviderTest {
+
+    private SdcSessionContextProvider sdcSessionContextProvider;
+
+    @Before
+    public void init() {
+        sdcSessionContextProvider = new SdcSessionContextProvider();
+    }
+
+    @Test(expected = SessionException.class)
+    public void createSessionNoTenant() {
+        sdcSessionContextProvider.create("user", null);
+        sdcSessionContextProvider.get();
+    }
+
+    @Test(expected = SessionException.class)
+    public void createSessionNoUser() {
+        sdcSessionContextProvider.create(null, "t1");
+        sdcSessionContextProvider.get();
+
+    }
+
+    @Test
+    public void createSessionSuccess() {
+        String user = "user";
+        String tenant = "tenant";
+        sdcSessionContextProvider.create(user, tenent);
+        SessionContext sessionContext = sdcSessionContextProvider.get();
+        assertEquals(user, sessionContext.getUserId());
+        assertEquals(tenent, sessionContext.getTenant());
+    }
+
+    @After
+    public void cleanup() {
+        sdcSessionContextProvider.close();
+    }
+}
