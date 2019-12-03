3af022aaa3ea39de9bd8f8f658f3693d7595b662
shrek2000 <oren.kleks@amdocs.com>
2019-12-03 15:11:52 +0200

    Increase code coverage
    test ZusammenConnector
    Issue-ID: SDC-2690
    
    Signed-off-by: shrek2000 <oren.kleks@amdocs.com>
    Change-Id: I18172cdd45d9160acabb8370c62ac47f279e99df
    Signed-off-by: shrek2000 <oren.kleks@amdocs.com>

---
+++ zusammen-lib/src/test/java/org/onap/sdc/common/zusammen/config/ZusammenConfigTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright Â© 2019 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.sdc.common.zusammen.config;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ZusammenConfigTest {
+
+    private static final String a = "a";
+    private static final String emptyStr = "";
+
+    @Test
+    public void TestConfig() {
+        ZusammenConfigProvider provider = Mockito.spy(getZusammenConfigProvider(a));
+        ZusammenConfig zusammenConfig = new ZusammenConfig(provider);
+        zusammenConfig.init();
+        Mockito.verify(provider).getCassandraAddresses();
+        Assert.assertEquals(a, System.getProperty("cassandra.nodes"));
+    }
+
+    @After
+    public void cleanUp() {
+        System.setProperty("cassandra.nodes", emptyStr);
+        System.setProperty("cassandra.port", emptyStr);
+        System.setProperty("cassandra.keyspace", emptyStr);
+
+        System.setProperty("cassandra.authenticate", emptyStr);
+        System.setProperty("cassandra.user", emptyStr);
+        System.setProperty("cassandra.password", emptyStr);
+
+        System.setProperty("cassandra.ssl", emptyStr);
+        System.setProperty("cassandra.truststore", emptyStr);
+        System.setProperty("cassandra.truststore.password", emptyStr);
+    }
+
+    private ZusammenConfigProvider getZusammenConfigProvider(String a) {
+        return new ZusammenConfigProvider() {
+            @Override
+            public String getCassandraAddresses() {
+                return a;
+            }
+
+            @Override
+            public String getCassandraPort() {
+                return a;
+            }
+
+            @Override
+            public String getCassandraAuth() {
+                return Boolean.FALSE.toString();
+            }
+
+            @Override
+            public String getCassandraUser() {
+                return a;
+            }
+
+            @Override
+            public String getCassandraPassword() {
+                return a;
+            }
+
+            @Override
+            public String getCassandraSSL() {
+                return Boolean.FALSE.toString();
+            }
+
+            @Override
+            public String getCassandraTrustStorePath() {
+                return a;
+            }
+
+            @Override
+            public String getCassandraTrustStorePassword() {
+                return a;
+            }
+        };
+    }
+
+
+}
