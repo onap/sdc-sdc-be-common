745e3fe26aa61ca02ea3a05e26b900671f5e04a6
m.kowalski3 <m.kowalski3@partner.samsung.com>
2020-05-21 08:01:47 +0200

    Add unit test for SecurityLogsUtils
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I9e235cb0569c29b4b0f8237fed3538758f4d8bf7

---
+++ security-util-lib/src/test/java/org/onap/sdc/security/util/SecurityLogsUtilsTest.java
@@ -0,0 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2020 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.sdc.security.util;
+
+import org.junit.Test;
+import org.onap.sdc.security.logging.elements.ErrorLogOptionalData;
+import org.onap.sdc.security.utils.SecurityLogsUtils;
+
+import static org.junit.Assert.assertNotNull;
+import static org.onap.sdc.security.utils.SecurityLogsUtils.PORTAL_TARGET_ENTITY;
+
+public class SecurityLogsUtilsTest {
+
+    @Test
+    public void testFullOptionalData() {
+
+        ErrorLogOptionalData errorLogOptionalData =
+            SecurityLogsUtils.fullOptionalData(PORTAL_TARGET_ENTITY, "/fetchRolesFromPortal");
+
+        assertNotNull(errorLogOptionalData);
+
+
+    }
+}
